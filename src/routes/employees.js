7c07e2e39d27e602b93f7454bf6576b9a9ed4ee2
realzam <relampago5032@gmail.com>
2020-04-26 20:44:15 -0500

    make it better

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -41,23 +41,67 @@ router.delete('/:id', (req, res) => {
 });
 
 // INSERT An Employee
-//0 alumno
-//1 docente
-//2 adminsitrador
+//1 alumno
+//2 docente
+//3 adminsitrador
 router.post('/add/alumno', (req, res) => {
   console.log("body",req.body);
   
   const {id, correo, password} = req.body;
   console.log(id, correo, password);
-  const query ="insert into  Usuario (id,correo,tipoUsuario,password) values (?,?,?,?)"
-  mysqlConnection.query(query, [id, correo,1,password], (err, rows, fields) => {
-    if(!err) {
-      res.json({status: 'usuario Saved'});
+  const query ="select correo from Usuario where correo=?";
+  const query2 ="select id from Usuario where id=?";
+  const query3 ="insert into  Usuario (id,correo,tipoUsuario,password) values (?,?,?,?)";
+  
+  mysqlConnection.query(query, [correo], (err, rows, fields) => {
+    if(rows.length>0) {
+      console.log("El correo ya esta en registrado");
+      res.json({error: "El correo ya esta en registrado"});
+    } else if(!err){
+
+      mysqlConnection.query(query2, [id], (err2, rows2, fields2) => {
+        if(rows2.length>0) {
+          console.log("la boleta ya  esta en registrada");
+          res.json({error: "la boleta ya  esta registrada"});
+        } else if(!err2){
+
+          mysqlConnection.query(query3, [id,correo,1,password], (err3, rows3, fields3) => {
+            if(!err3) {
+              console.log("Usuario guardado");
+              res.json({status: "Usuario guardado"});
+            } else{
+              console.log(err3);
+              res.json({error: "ups hubo algun error :("});
+            }
+          });
+          
+        } else{
+          console.log(err2);
+          res.json({error: err2});
+        }
+      });
     } else {
       console.log(err);
+      res.json({error: err});
     }
   });
 
+
+});
+
+router.get('/login', (req, res) => {
+  const { id, password } = req.body;
+  
+  mysqlConnection.query('SELECT id FROM Usuario where id=? and password=?',[id,password], (err, rows, fields) => {
+    if(rows.length<1) {
+      res.json({error: "Usuario o Contraseña incorrecta"});
+    }if(!err) {
+      res.json({status: "ok"});
+    } else {
+      console.log(err);
+      res.json({error: "ups hubo algun error :("});
+    }
+  });  
 });
 
 router.put('/modifyAlumno/:id', (req, res) => {
@@ -66,7 +110,7 @@ router.put('/modifyAlumno/:id', (req, res) => {
   const query ="update  Usuario where nombre=?,apellido=?,correo=?,?"
   mysqlConnection.query(query, [id, name, salary], (err, rows, fields) => {
     if(!err) {
-      res.json({status: 'Employee Updated'});
+      res.json({status: "Usuario guardado"});
     } else {
       console.log(err);
     }
