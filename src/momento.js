491170792a676bbb048e8b5a6cb0a545cb736021
realzam <relampago5032@gmail.com>
2020-05-11 15:51:27 -0500

    make it better

--- src/momento.js
+++ src/momento.js
@@ -8,23 +8,23 @@ var init=null
 function momento()
 {   if(fecha==null)
    
-        return moment(moment( moment().tz("America/Mexico_City").format('YYYY-MM-DTHH:mm:ss.SSS')));
+        return moment(moment().tz("America/Mexico_City").format('YYYY-MM-DTHH:mm:ss.SSS'));
 
         try
         {
-            return moment(fecha+(moment(moment( moment().tz("America/Mexico_City").format('YYYY-MM-DTHH:mm:ss.SSS')))-init));
+            return moment(fecha+(moment(moment().tz("America/Mexico_City").format('YYYY-MM-DTHH:mm:ss.SSS'))-init));
         }catch(e)
         {
             console.log('error al crear la fecha')
             console.log(e)
-            return moment(moment( moment().tz("America/Mexico_City").format('YYYY-MM-DTHH:mm:ss.SSS')));
+            return moment(moment().tz("America/Mexico_City").format('YYYY-MM-DTHH:mm:ss.SSS'))
         }
     
     
 }
 function setFecha(param) {
     fecha=param;
-    init=moment(moment( moment().tz("America/Mexico_City").format('YYYY-MM-DTHH:mm:ss.SSS')))
+    init=moment(moment().tz("America/Mexico_City").format('YYYY-MM-DTHH:mm:ss.SSS'))
 }
 
 module.exports.momento=momento;
