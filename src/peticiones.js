f546c76d8d439cf9361eb52606f852e3d99f8b40
realzam <relampago5032@gmail.com>
2020-06-04 17:00:53 -0500

    reservaAdmin

--- src/peticiones.js
+++ src/peticiones.js
@@ -209,7 +209,7 @@ async function reservaTimeOut(date) {
 function getreservaTimeOutNotification(date) {
   var edo = "En espera";
   return new Promise(resolve => {
-    mysqlConnection.query('select u.tipoUsuario, r.idComputadora,r.idLaboratorio,t.idToken from ReservaComputadora r, TokenNotification t, Usuario u where r.fin=? and r.estado=? and r.idUsuario=t.usuario u.id=r.idUsuario', [date, edo], async (err, rows, fields) => {
+    mysqlConnection.query('select u.tipoUsuario, r.idComputadora,r.idLaboratorio,t.idToken from ReservaComputadora r, TokenNotification t, Usuario u where r.fin=? and r.estado=? and r.idUsuario=t.usuario and u.id=r.idUsuario', [date, edo], async (err, rows, fields) => {
       if (!err) {
         console.log('reservaTimeOutSendNotification', rows.length)
         if (rows.length > 0) {
